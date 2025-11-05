4dcda0aa94cf5743f86ecbf892c5222853114fd3
al1c333 <161310536+al1c333@users.noreply.github.com>
2025-11-04 22:38:34 -0300

    Implement image toggle feature on button click
    Added functionality to toggle image display on button click.

--- meu projeto/js/script.js
+++ meu projeto/js/script.js
@@ -1,1 +1,19 @@
 alert("Bem-vindo(a) à página!");
+
+    const botoes = document.querySelectorAll('.verImagem');
+
+    botoes.forEach(botao => {
+      botao.addEventListener('click', () => {
+        const container = botao.nextElementSibling;
+        const img = container.querySelector('img');
+        if (container.style.display === "none") {
+          img.src = botao.getAttribute('data-img');
+          container.style.display = "block";
+          botao.textContent = "Ocultar imagem";
+        } else {
+          container.style.display = "none";
+          img.src = "";
+          botao.textContent = "Ver imagem";
+        }
+        }
+                             };
